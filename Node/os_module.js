dc70b04028144df410df6282955ddc99a8876744
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-03-14 20:36:27 +0600

    .

---
+++ Node/os_module.js
@@ -0,0 +1,12 @@
+const os = require("os");
+console.log("Platform: " + os.platform());
+console.log("User: ", os.userInfo());
+console.log("CPU Architecture: " + os.arch());
+console.log("Free Memory: " + os.freemem(), "bytes");
+console.log("Total Memory: " + os.totalmem(), "bytes");
+console.log("System Uptime: " + os.uptime(), "seconds");
+console.log("Home Directory: " + os.homedir());
+console.log("Host Name: " + os.hostname());
+console.log("OS Type: " + os.type());
+console.log("Network Interfaces: ", os.networkInterfaces());
+console.log("CPUs: ", os.cpus());
