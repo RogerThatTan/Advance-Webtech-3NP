5aaff3fccd125f5fc4daab4efc103b15ad6f97bb
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-03-14 16:52:18 +0600

    init

---
+++ Node/app.js
@@ -0,0 +1,10 @@
+// const add = require("./math");
+
+// const { add, mult, subs, div, PI } = require("./math");
+const math = require("./math");
+
+console.log(math.add(5, 10));
+console.log(math.mult(5, 10));
+console.log(math.subs(5, 10));
+console.log(math.div(5, 10));
+console.log(math.PI);
---
+++ Node/math.js
@@ -0,0 +1,28 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subs = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const mult = (a, b) => {
+  return a * b;
+};
+
+const PI = 3.124;
+
+// module.exports.add = add;
+// module.exports.mult = mult;
+
+module.exports = {
+  add,
+  mult,
+  subs,
+  div,
+  PI,
+};
