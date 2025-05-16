ae620db91afcaa2aed0bc2ddc7c52e2f00adce54
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-05-16 22:41:09 +0600

    V2-Init

---
+++ React/my-react-V2-app/src/components/TodoClass.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+class Todo extends React.Component {    
+    state = {
+        todo: " ",
+        warning:null,
+    };
+
+    handleInput = (e) => {
+        const inputValue = e.target.value;
+        const warning = inputValue.includes('.js')? 'Please do not include .js in the todo' : null;
+        this.setState({
+            todo : inputValue,
+            warning,
+        });
+    };
+    render() {
+        const{ todo, warning } = this.state;
+        return (
+            <div>
+               <p>{todo}</p>
+               <p>
+                <textarea name="todo" value={todo} onChange={this.handleInput}/>
+               </p>
+               <hr />
+               <h2>{warning || 'Good Choice'}</h2>
+            </div>
+        );
+    }
+}
+export default Todo;
---
+++ React/my-react-V2-app/src/lib/Note-V2.jsx
@@ -0,0 +1,47 @@
+// Why React doesnt prefer inheritance?
+// 1. Components are tightly coupled.
+// 2. From child it is not clear what parent does.
+// 3. Not clear about the parent child relationship from the App.jsx.
+// 4. Future components are also tightly coupled.
+// 5. Nested extend - child that already extends other parent class.
+
+// -------------------------------------------------------------------------
+
+// Why React prefers composition?
+// Firstly we will recieve the function as props not the class inheritance.
+// There will be no depenedency on the parent class.
+// So basically we are doing Composition over Inheritance to avoid the problems of inheritance.
+// 1. Components are loosely coupled.
+// 2. From child it is clear what parent does.
+// 3. Clear about the parent child relationship from the App.jsx.
+// 4. Future components are not tightly coupled.
+// 5. No nested extend - child that already extends other parent class.
+// 6. No need to worry about the order of the parent class.
+
+// -------------------------------------------------------------------------
+
+//What is HOC?
+// 1. HOC is a function that takes a component as an argument and returns a new component.
+// 2. HOC is a pattern that is used to reuse component logic.
+// 3. HOC is not a part of the React API.
+// 4. HOC is a pure function.
+// 5. HOC is a higher order function.
+// 6. HOC is a function that takes a component as an argument and returns a new component.
+
+// -------------------------------------------------------------------------
+
+//What is Render Prop?
+// Render prop is a prop that define the waht component will render.
+// Render prop is a function prop.
+// Render prop is a pattern that is used to share code between components.
+
+// -------------------------------------------------------------------------
+
+//What is Context API?
+// Context API is a way to pass data through the component tree without having to pass props down manually at every level.
+
+// -------------------------------------------------------------------------
+
+//Why React Hooks? What is the need of Hooks?
+// 1. Hooks are a new way to use state and other React features without writing a class.
+// 2. Hooks are a way to reuse stateful logic between components.
